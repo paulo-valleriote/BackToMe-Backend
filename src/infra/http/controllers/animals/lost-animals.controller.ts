e3149a2d46435ec9bf9d43a8779e0b4ecdb6752d
Paulo Valleriote <paulomv29@outlook.com>
2023-06-15 11:29:34 -0300

    rota para registro de animal perdido

--- src/infra/http/controllers/animals/lost-animals.controller.ts
+++ src/infra/http/controllers/animals/lost-animals.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { RegisterLostAnimalDTO } from '@infra/http/dtos/LostAnimal/registerLostAnimal.dto';
+import { LostAnimalService } from '@infra/http/services/animals/lost-animals.service';
+import { Body, Controller, Get } from '@nestjs/common';
 
 @Controller('/lost-animals')
 export class LostAnimalsController {
-  constructor() {}
+  constructor(private lostAnimalsSerivce: LostAnimalService) {}
 
   @Get('/registered')
-  async register() {
-    return {};
+  async register(@Body() registerLostAnimalDTO: RegisterLostAnimalDTO) {
+    return this.lostAnimalsSerivce.register(registerLostAnimalDTO);
   }
 }
