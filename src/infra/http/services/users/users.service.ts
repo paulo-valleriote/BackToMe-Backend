08acba061d8ec1b749888df611b398b8a3dfc005
Paulo Valleriote <paulomv29@outlook.com>
2023-06-17 19:39:33 -0300

    criando service de login para o usuário

--- src/infra/http/services/users/users.service.ts
+++ src/infra/http/services/users/users.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UserRepository } from '@app/repositories/User/user';
 import { User } from '@domain/User/User';
 import { CpfValidator } from '@app/protocols/cpf/cpfValidator';
 import { PhoneValidator } from '@app/protocols/phone/phoneValidator';
 import { InvalidParamError } from '@app/errors/InvalidParamError';
+import { UserLoginDTO } from '@infra/http/dtos/User/login.dto';
+import { z } from 'zod';
 
 interface RegisterUserRequest {
   name: string;
@@ -38,5 +40,29 @@ export class UserService {
 
     await this.userRepository.register(newUser);
     return newUser;
+  }
+
+  async login(request: UserLoginDTO): Promise<string | Error> {
+    const requestSchema = z.object({
+      email: z.string().email().min(6, { message: 'Invalid' }),
+      password: z.string(),
+    });
+
+    const loginProps = requestSchema.safeParse(request);
+
+    if (!loginProps.success) {
+      throw new BadRequestException('User login error', {
+        cause: new BadRequestException(),
+        description: loginProps.error.errors[0].message,
+      });
+    }
+
+    const userLoginResponse = await this.userRepository.login(loginProps.data);
+
+    if (userLoginResponse instanceof BadRequestException) {
+      throw userLoginResponse;
+    }
+
+    return userLoginResponse;
   }
 }
