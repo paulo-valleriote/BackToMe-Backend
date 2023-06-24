36b2259c1897d999bc71d683c6937e2b25b0a355
Paulo Valleriote <paulomv29@outlook.com>
2023-06-24 09:50:46 -0300

    bugfix validação - calculo correto implementado

--- src/app/protocols/cpf/cpfValidator.ts
+++ src/app/protocols/cpf/cpfValidator.ts
@@ -1,5 +1,9 @@
 export class CpfValidator {
   execute(cpf: string): boolean {
+    if (!cpf) {
+      throw new Error('None CPF provided');
+    }
+
     const formatedCpf = cpf.replace(/[^\d]+/g, '');
 
     if (formatedCpf.length !== 11 || !!formatedCpf.match(/(\d)\1{10}/))
@@ -8,9 +12,9 @@ export class CpfValidator {
     const rest = (count: number) => {
       const slicedCpf = formatedCpf.slice(0, count - 12);
       const digitArray = slicedCpf.split('').map((digit) => Number(digit));
-      const digitSum = digitArray.reduce(
-        (soma, el, index) => soma + el * (count - index) * 10,
-      );
+      const digitSum =
+        digitArray.reduce((soma, el, index) => soma + el * (count - index), 0) *
+        10;
       const cpfRest = (digitSum % 11) % 10;
 
       return cpfRest.toString();
