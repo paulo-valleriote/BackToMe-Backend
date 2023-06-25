73531c4e0ed5ed87ef0a2bd84879f04e30b3ac35
Paulo Valleriote <paulomv29@outlook.com>
2023-06-25 15:34:36 -0300

    adicionando envio de emails e gerenciamento de fila com redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,32 +5,47 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import env from './env';
+import { MailModule } from '@infra/http/controllers/mail/mail.module';
 
 @Module({
-  imports: [HttpModule, DatabaseModule],
-  controllers: [MessageController],
-})
-@Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
-    BullModule.forRoot({
-      redis: {
-        host: process.env.QUEUE_HOST,
-        port: Number(process.env.QUEUE_PORT),
-      },
-    }),
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.MAILER_HOST,
-        port: process.env.MAILER_PORT,
-        auth: {
-          user: process.env.MAILER_USER,
-          pass: process.env.MAILER_PASS,
+    ConfigModule.forRoot(),
+    BullModule.forRootAsync({
+      useFactory: () => ({
+        redis: {
+          host: env.QUEUE_HOST,
+          port: Number(env.QUEUE_PORT),
+          username: env.QUEUE_USER,
+          password: env.QUEUE_PASSWORD,
+          tls: {
+            rejectUnauthorized: true,
+          },
         },
-      },
+      }),
     }),
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        defaults: {
+          from: 'Equipe BackToMe',
+        },
+        catch: (err) => console.log(err),
+        finally: () => console.log('email has been sent'),
+        transport: {
+          host: env.MAILER_HOST,
+          port: Number(env.MAILER_PORT),
+          auth: {
+            user: env.MAILER_USER,
+            pass: env.MAILER_PASS,
+          },
+        },
+      }),
+    }),
+    MailModule,
     HttpModule,
     DatabaseModule,
   ],
+  controllers: [MessageController],
+  providers: [],
 })
 export class AppModule {}
