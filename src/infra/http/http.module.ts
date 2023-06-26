f8fdd7210c8e6602aa3c06134e89b989f1f36452
Paulo Valleriote <paulomv29@outlook.com>
2023-06-26 15:48:47 -0300

    refatorando organização e estrutura do código

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -4,14 +4,14 @@ import { UsersModule } from './controllers/users/user.module';
 import { FoundAnimalsModule } from './controllers/animals/found-animals.module';
 import { AdoptionAnimalsModule } from './controllers/animals/adoption-animals.module';
 import { ImagesModule } from './controllers/images/images.module';
-import { MailModule } from './controllers/mail/mail.module';
+import { MailQueueModule } from './controllers/mail/mail.module';
 
 @Module({
   imports: [
     LostAnimalsModule,
     UsersModule,
     FoundAnimalsModule,
-    MailModule,
+    MailQueueModule,
     AdoptionAnimalsModule,
     ImagesModule,
   ],
