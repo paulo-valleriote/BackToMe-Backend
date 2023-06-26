09e3063e9915581bc167c864c42cb3214620af17
Paulo Valleriote <paulomv29@outlook.com>
2023-06-26 15:49:13 -0300

    continuando integragração do envio de emails com gerenciamento de queue

--- src/infra/http/services/mail/mail.service.ts
+++ src/infra/http/services/mail/mail.service.ts
@@ -1,22 +1,34 @@
 import { PasswordRecoveryMailDTO } from '@infra/http/dtos/User/passwordRecovery.dto';
 import { MailerService } from '@nestjs-modules/mailer';
-import { Process, Processor } from '@nestjs/bull';
+import {
+  OnQueueCompleted,
+  OnQueueError,
+  Process,
+  Processor,
+} from '@nestjs/bull';
 import { Job } from 'bull';
+import env from 'src/env';
 
 @Processor('mail')
 export class MailProcessorService {
   constructor(private mailService: MailerService) {}
   @Process('mail-job')
   sendMailJob(job: Job<PasswordRecoveryMailDTO>) {
-    this.mailService
-      .sendMail({
-        headers: {},
-        to: job.data.email,
-        from: '<paulovalleriotedev@gmail.com>',
-        subject: 'Redefinição de senha',
-        html: `Olá! Nós sentimos sua falta, <a href={${job.data.recoveryLink}} clique aqui</a> para redefinir sua senha\n\nBem vindo(a) de volta!`,
-      })
-      .then(() => console.log('Email enviado para: ', job.data.email))
-      .catch((err) => console.log(err));
+    this.mailService.sendMail({
+      to: job.data.email,
+      from: env.MAILER_SENDER,
+      subject: 'Redefinição de senha',
+      html: `Olá! Nós sentimos sua falta, <a href={${job.data.recoveryLink}} clique aqui</a> para redefinir sua senha\n\nBem vindo(a) de volta!`,
+    });
+  }
+
+  @OnQueueCompleted()
+  logCompletedQueue(job: Job) {
+    console.log('E-mail foi enviado!', job.data.email);
+  }
+
+  @OnQueueError()
+  logQueueError(job: Job) {
+    console.log('O E-mail não pode ser enviado, motivo:', job.failedReason);
   }
 }
