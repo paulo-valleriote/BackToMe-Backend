87a7a025d1e3eb5cf3b2e5bbfe73cea01b08430d
Sergio Bastos <sergiobastosfisio@gmail.com>
2023-07-30 19:09:44 -0300

    file upload

--- src/infra/http/controllers/images/images.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ImagesController } from './images.controller';
-import { FileService } from '@infra/http/services/images/images.service';
-
-@Module({
-  controllers: [ImagesController],
-  providers: [FileService],
-})
-export class ImagesModule {}
