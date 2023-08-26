2a1f04705b422f1b22602910e458da5ac9ece23c
Marcus Roza <marcusroza@pop-os.localdomain>
2023-08-26 18:35:49 -0300

    problema no cors

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['https://btm-backend.onrender.com', 'http://localhost:5173','https://back-to-me.vercel.app/'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-
-  });
-
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(3000,()=>console.log("BackToMe. está online"));
 }
 bootstrap();
