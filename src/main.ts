feca58527f7872747f426d1018b62200ff23a158
Marcus Roza <marcusroza@pop-os.localdomain>
2023-08-26 14:18:39 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://btm-backend.onrender.com', 'http://localhost:5173','https://back-to-me.vercel.app/'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
